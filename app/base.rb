709081ffa8ae455676b459d93acceb5047eb81dd
OpenShift guest <ngsmrk@gmail.com>
2014-01-11 17:48:04 -0500

    Example working

--- app/base.rb
+++ app/base.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'haml'
 require 'httparty'
 
+require_relative 'wunderground_client'
+
 get '/' do
     client = WundergroundClient.new('463b65ffdd4cff1c')
     response = client.holiday_planner_info('0701', '0731', 'France', 'Paris')
