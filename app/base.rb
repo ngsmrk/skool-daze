7b31989f31ab187e78b94d7b0b06956f050bbf1e
OpenShift guest <ngsmrk@gmail.com>
2014-01-11 20:04:52 -0500

    1st stab at search form

--- app/base.rb
+++ app/base.rb
@@ -5,9 +5,9 @@ require 'httparty'
 require_relative 'wunderground_client'
 
 get '/' do
-    client = WundergroundClient.new('463b65ffdd4cff1c')
-    response = client.holiday_planner_info('0701', '0731', 'France', 'Paris')
-    puts response.keys
+    # client = WundergroundClient.new('463b65ffdd4cff1c')
+    # response = client.holiday_planner_info('0701', '0731', 'France', 'Paris')
+    # puts response.keys
 
     haml :index
 end
