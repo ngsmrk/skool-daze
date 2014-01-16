c079bb2fb9d42d2d341eac5b5c114ddd7b95557d
Angus Mark <ngsmrk@gmail.com>
2014-01-16 17:09:03 +0000

    Search form is now in a partial

--- app/base.rb
+++ app/base.rb
@@ -9,8 +9,7 @@ end
 
 post '/' do
   client = WundergroundClient.new('463b65ffdd4cff1c')
-  response = client.planner(params['start_date'], params['end_date'], params['country'], params['town_city'])
-  @title = response.title
+  @client_response = client.planner(params['start_date'], params['end_date'], params['country'], params['town_city'])
 
-  haml :results  
+  haml :results
 end
