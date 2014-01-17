a5d596a246067c197cff3cb1a0a7d56e2ac2be53
Angus Mark <ngsmrk@gmail.com>
2014-01-17 15:18:11 +0000

    Now uses country dropdown

--- app/base.rb
+++ app/base.rb
@@ -1,15 +1,22 @@
 require 'sinatra'
 require 'haml'
+require 'iso_country_codes'
 
 require_relative 'wunderground_client'
 
 get '/' do
+  @countries = countries
   haml :index
 end
 
 post '/' do
   client = WundergroundClient.new('463b65ffdd4cff1c')
   @client_response = client.planner(params['start_date'], params['end_date'], params['country'], params['town_city'])
+  @countries = countries
 
   haml :results
+end
+
+def countries
+  IsoCountryCodes.for_select
 end
